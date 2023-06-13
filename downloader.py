93179e3d7f3ffeaf7ab9e41d6f599b949696709e
SodaYodb <95384979+SodaYodb@users.noreply.github.com>
2023-06-13 23:05:26 +0200

    Update downloader.py
    work in progress

--- downloader.py
+++ downloader.py
@@ -1,57 +1,57 @@
+from bs4 import BeautifulSoup
 import requests
-import threading
-import os
+import re
 
-path_to_save = "RIS2 PDF/"
-start_nbr = 250  # first is
-end_nbr = 310000
-batch_size = 10000
-num_threads = (end_nbr - start_nbr) // batch_size
+cjahr = "2020"
+cmonat = "1"
+canz = "12" # months ahead - more is possible but ofc slower
+time_pattern = r"\d{2}:\d{2}"
 
-# for check_new
-distance_new = 1000
+def get_list_with_meta():
+    address = "https://sessionnet.dessau.de/bi/si0046.asp?__cjahr=" + cjahr + "&__canz=" + canz + "&__cmonat=" + cmonat
+    response = requests.get(address)
+    print(response.status_code)
+    html_text = response.text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    full_list = []
 
-lock = threading.Lock()
-
-
-def download_files(start, end):
-    for i in range(start, end):
-        address = "https://sessionnet.dessau.de/bi/getfile.asp?id=" + str(i) + "&type=do"
-        print(address)
-        response = requests.get(address)
-        if response.status_code == 200:
-            filename = response.headers.get('content-disposition')
-            filename = str(i) + filename[filename.find('"') + 1:-11]
-            with lock:
-                open(path_to_save + filename, 'w').write(response.content)
-        else:
-            print(response.status_code)
+    last_event_date = ""
+    for table_record in soup.find_all('tr'):
+        if table_record.find('th') is not None:  # ignore table head
+            continue
+        down_elements = []
+        event_date = ""
+        other_meta = []
+        for record_data in table_record.find_all('td'):
+            class_attr = record_data.get('class')
+            if class_attr is not None:
+                if 'sidat_tag' in class_attr:
+                    if record_data.a is not None:
+                        event_date = record_data.a.span.next_sibling
+                        last_event_date = event_date
+                    else:
+                        event_date = last_event_date
+                if 'silink' in class_attr:
+                    if record_data.a is not None:
+                        other_meta.append(record_data.a.text)
+                        for list_elements in record_data.find_all('li'):
+                            if re.search(time_pattern, list_elements.text):
+                                other_meta.append(list_elements.text[:5])
+                            else:
+                                other_meta.append(list_elements.text)
+                if 'sidocs' in class_attr:
+                    if record_data.a is not None:
+                        a_ele = record_data.find_all('a')
+                        for ele in a_ele:
+                            if ele not in down_elements:
+                                down_elements.append(ele.get('href'))
+        if down_elements:
+            full_list.append([event_date, other_meta, down_elements])
+    return full_list
 
 
 def main():
-    # is empty
-    if len(os.listdir(path_to_save)) == 0:
-        threads = []
-        for i in range(num_threads):
-            start = start_nbr + (i * batch_size)
-            end = start + batch_size
-            thread = threading.Thread(target=download_files, args=(start, end))
-            print("thread ", start, end)
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-        print("All threads have finished downloading.")
-    else:
-        # single thread because we have time?
-        lst_folder_items = os.listdir(path_to_save)
-        highest_number = 0
-        for item in lst_folder_items:
-            act_number = int(item[:item.find("-")])
-            if act_number > highest_number:
-                highest_number = act_number
-        download_files(highest_number, (highest_number + distance_new))
+    print(get_list_with_meta())
 
 
 if __name__ == "__main__":
