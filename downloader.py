7622e4d23bd6283856ac4bd32c8c3b6239ee967b
SodaYodb <95384979+SodaYodb@users.noreply.github.com>
2023-06-26 17:39:41 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,5 +1,4 @@
 from sessionnet_scraper import SessionnetCrawler
-from down_from_json import load_edit
 
 crawler = Sessionnet_Crawler("RIS2_PDF/", "Dessau", "https://sessionnet.dessau.de/bi/info.asp", 2022, 1, 12, False)
 crawler.start_crawl()
