649fbc3ab6c5189891506c2ba134b2d4bd9498dc
SodaYodb <95384979+SodaYodb@users.noreply.github.com>
2023-09-19 11:50:25 +0200

    local_website.py
    preview of found content now shown on website

--- local_website.py
+++ local_website.py
@@ -27,30 +27,41 @@ def get_search():
     query = request.form.get('in_search')
 
     if re.match(r'^[\w\s]+$', query):
+
         body = {
             "query": {
                 "multi_match" : {
                     "query":      query,
                     "type":       "best_fields",
                     "fields":     [ "*" ]
-                }}}
+                }},
+                "highlight": {
+                    "fields": {
+                        "main_files.parsed_data.content": {},
+                        "sub_sessions.sub_files.parsed_data.content": {}
+                    }}}
         response = es.search(index="test_local", body=body)
 
         if response['hits']['total']['value'] >= 1:
             for hit in response['hits']['hits']:
                 item = {}
-                print(hit['_score'])
-                print(base_website + hit['_source']['meeting']['link'])
-                print(hit['_source']['meeting']['date'])
-
                 item["date"] = hit['_source']['meeting']['date']
                 item["accuracy"] = hit['_score']
                 item["link"] = base_website + hit['_source']['meeting']['link']
                 item["resp"] = hit['_source']['meeting']['responsible']
+                preview = hit['highlight']
+                n_content = ""
                 try:
-                    item["desc"] = hit['_source']['main_files']['name']
-                except TypeError:
-                    item["desc"] = "Error"
+                    for content in preview["sub_sessions.sub_files.parsed_data.content"]:
+                        print(content)
+                        n_content += str(content)
+                except:
+                    for content in preview["main_files.parsed_data.content"]:
+                        n_content += str(content)
+                n_content = n_content.replace("\n", "")
+                n_content = n_content.replace("<em", "")
+                n_content = n_content.replace("/em>", "")
+                item["desc"] = n_content
                 items.append(item)
         else:
             item = {}
@@ -58,9 +69,8 @@ def get_search():
             items.append(item)
     else:
         item = {}
-        item["date"] = "query dos not meet pattern a-zA-Z0-9_"
+        item["date"] = "querry must meet pattern a-zA-Z0-9_ "
         items.append(item)
-
 
     return render_template("index.html", items = items)
 
