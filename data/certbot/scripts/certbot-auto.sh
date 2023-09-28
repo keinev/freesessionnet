38cea7999b0dbb5d2ca30ccd3dad7e7fe9f3ff9d
SodaYodb <95384979+SodaYodb@users.noreply.github.com>
2023-09-28 22:09:17 +0200

    fixed_certbot
    now certbot loading from .env

--- data/certbot/scripts/certbot-auto.sh
+++ data/certbot/scripts/certbot-auto.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-DOMAIN=""
-WWW_DOMAIN=""
+source /scripts/.env
+
+DOMAIN=$DOMAIN
+WWW_DOMAIN=$WWWDOMAIN
 CERT_PATH="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
-EMAIL=""
+EMAIL=$EMAIL
 if [ ! -f $CERT_PATH ]; then
     certbot certonly --webroot -w /var/www/certbot -d $DOMAIN -d $WWW_DOMAIN --non-interactive --agree-tos --email $EMAIL
 fi
